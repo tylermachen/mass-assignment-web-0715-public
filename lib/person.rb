310c38f7e539812d04d95808c7fe2ec1881f12b8
Tyler Machen <tylermachen@gmail.com>
2015-08-10 18:12:38 -0400

    add meta programming madness

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,29 @@
+# meta programming below!
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
-                :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
-                :glove_size, :pant_length, :pant_width
-
-  def initialize(attributes = {})
+  def initialize(attributes)
     attributes.each do |key, value|
-      self.class.send(:define_method, key) { value }
+      # instance_variable_set("@#{key}", value)
+      self.send("#{key}=", value)
+    end
+  end
+
+  # create getters:
+  # def name
+  #   @name
+  # end
+  [:name, :birthday, :hair_color, :eye_color, :height,
+    :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
+    :glove_size, :pant_length, :pant_width].each do |meth|
+    define_method "#{meth}" do
+      instance_variable_get("@#{meth}")
+    end
+
+    # create setters:
+    # def name=(new_name)
+    #   @name = new_name
+    # end
+    define_method "#{meth}=" do |new_value|
+      instance_variable_set("@#{meth}", new_value)
     end
   end
 end
